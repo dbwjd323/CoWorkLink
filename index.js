4b521f1749ff3903855377f8e0bfe24e3321ba1c
roor <02tladbwjd@naver.com>
2023-11-04 19:06:16 +0900

    CoWorkLink git 연결

---
+++ index.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var ejs = require('ejs');
+var mysql = require('mysql');
+var express = require('express');
+var bodyParser = require('body-parser');
+const path = require('path');
+
+var client = mysql.createConnection({
+    user: 'root',
+    password: '1234',
+    database: 'CoWorkLink'
+});
+
+var app = express();
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use("/public", express.static(path.join(__dirname,"public")));
+
+app.listen(3001, function(){
+    console.log('Server running at http://127.0.0.1:3001');
+});
+
+//라우트 실행
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pages/index.html'));
+  });
