acb9a1e126583797732adbeba8a118da6a9bb594
roor <02tladbwjd@naver.com>
2023-11-06 23:48:55 +0900

    회원가입 기능구현

--- index.js
+++ index.js
@@ -29,7 +29,41 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/pages/index.html'));
   });
 
-  app.get('/pages/login.html', function(req, res) {
+  app.get('/login', function(req, res) {
     res.sendFile(path.join(__dirname, '/pages/login.html'));
 });
 
+app.get('/join', function(req, res) {
+    res.sendFile(path.join(__dirname, '/pages/join.html'));
+});
+
+// ...
+
+app.post('/join', function(req, res) {
+    var username = req.body.username;
+    var userID = req.body.userID;
+    var password = req.body.password;
+    var passwordConfirm = req.body.passwordConfirm;
+    var email = req.body.email;
+
+    if (password !== passwordConfirm) {
+        // 비밀번호가 일치하지 않을 경우 에러 메시지와 함께 다시 회원가입 페이지로 리다이렉트합니다.
+        return res.redirect('/join?error=비밀번호가 일치하지 않습니다. 다시 시도해주세요.&username=' + username + '&userID=' + userID + '&email=' + email);
+    }
+
+    // 비밀번호 확인이 일치할 경우에만 회원가입을 처리합니다.
+    var sql = "INSERT INTO users (username, userID, password, passwordConfirm, email) VALUES (?, ?, ?, ?, ?)";
+    client.query(sql, [username, userID, password, passwordConfirm, email], function(error, results, fields) {
+        if (error) {
+            console.log(error);
+            res.send('회원가입에 실패했습니다.');
+        } else {
+            var alertMessage = "회원가입이 완료되었습니다!";
+            // Display the alert message and then redirect to the login page
+            res.setHeader('Content-Type', 'text/html; charset=utf-8');
+            res.write('<script>alert("' + alertMessage + '");</script>');
+            res.write('<script>window.location.href="/login";</script>');
+            res.end();
+        }
+    });
+});
